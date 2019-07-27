dadc3532a4aa9d9385d8ec4e29aad02d3574bcc5
alexdd1414 <debayodoherty@college.harvard.edu>
2019-07-26 23:14:24 -0400

    working on database

--- connections/models.py
+++ connections/models.py
@@ -3,13 +3,37 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+class Year(models.Model):
+    
+
+    year_in_school = models.CharField(
+        max_length=2, 
+        choices = YEAR_IN_SCHOOL_CHOICES,
+        default=FRESHMAN,
+    )
+
 class Player(models.Model):
-    player_text = models.CharField(max_length=200)
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = "SR"
+    YEAR_IN_SCHOOL_CHOICES = [
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+    ]
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length = 40, primary_key=True)
+    email = models.EmailField().db_index = True
+    year = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICESs)
+    
+
+
     pub_date = models.DateTimeField('data published')
     def __str__(self):
         return self.player_text
-    # def was_published_recently(self):
-    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Position(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
